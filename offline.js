5e3551574128c35a3566c6f2fb03d02068563bb2
nuttawat3005 <52026431+nuttawat3005@users.noreply.github.com>
2019-07-11 10:30:44 +0700

    fruitgame

---
+++ offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1562815823,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/fruit-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/bad-sheet0.png",
+		"images/basket-sheet0.png",
+		"images/block-sheet0.png",
+		"images/sprite4-sheet0.png",
+		"images/sprite6-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
